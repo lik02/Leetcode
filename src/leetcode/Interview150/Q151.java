861f036a01865f63f6cd96064c4d2fa88c7f2c95
Yu Lik <likyooi18@gmail.com>
2024-06-06 03:02:37 +0800

    add

--- src/leetcode/Interview150/Q151.java
+++ src/leetcode/Interview150/Q151.java
@@ -3,10 +3,10 @@ package leetcode.Interview150;
 public class Q151 {
     public String reverseWords(String s) {
         String result = "";
-        String[] str = s.split(" ");
+        String[] str = s.split(" ");// if "  " two space, it gives "","","", three empty strings
         for (int i = str.length - 1; i >= 0; i--) {
-            if (!str[i].equals("")) {
-                result += (result.equals("") ? str[i] : " " + str[i]);
+            if (!str[i].equals("")) {// avoid "" because it will concatenate space in result
+                result += (result.equals("") ? str[i] : " " + str[i]);// for the first word, it will not write space, but the rest will
             }
         }
         return result;
