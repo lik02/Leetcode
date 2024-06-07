9e1278c70c6eb7418dae00c26497bb5eb4a932a9
Yu Lik <likyooi18@gmail.com>
2024-06-07 15:43:06 +0800

    add

---
+++ src/leetcode/Interview150/Q648.java
@@ -0,0 +1,23 @@
+package leetcode.Interview150;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Q648 {
+    public String replaceWords(List<String> dictionary, String sentence) {
+        String[] str = sentence.split(" ");
+        String result = "";
+        for (String i: dictionary) {
+            for (int j = 0; j < str.length; j++) {
+                if (str[j].startsWith(i) && i.length() < str[j].length()) {
+                    str[j] = i;
+                }
+            }
+        }
+        for (int i = 0; i < str.length; i++) {
+            result += (i != 0 ? " " + str[i] : str[i]);
+        }
+        return result;
+    }
+}
